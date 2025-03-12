a8656c01da61db1c036e0b79072d619ee43a8e83
juliansantamariaV <santamariavalenciajulianv@gmail.com>
2025-03-12 15:25:06 -0500

    v2

--- src/OnePagePortfolio.tsx
+++ src/OnePagePortfolio.tsx
@@ -7,8 +7,10 @@ import { Toaster } from "sonner";
 import { InputForm } from "./components/Form";
 
 
+import { technologies } from "./utils/technologies";
 
 export default function OnePagePortfolio() {
+
   const name: string = "Julian";
 
   const sectionIds = ["home", "about", "projects", "contact"];
@@ -26,18 +28,31 @@ export default function OnePagePortfolio() {
         id="home"
         className="h-screen flex items-center justify-center bg-gradient-to-r from-gray-900 to-gray-800"
       >
-        <h1 className="text-4xl font-semibold home light">Hi, i'm {name}.</h1>
+        <h1 className="text-4xl font-semibold light">Hi, i'm {name}.</h1>
 
         <div className="absolute bottom-4 right-4 flex flex-col justify-center items-center">
-    <ArrowDownIcon className="w-8 h-8 text-zinc-200" />
-  </div>
+          <ArrowDownIcon className="w-8 h-8 text-zinc-200" />
+        </div>
       </section>
 
       <section
         id="about"
-        className="h-screen flex items-center justify-center bg-gradient-to-r from-gray-300 to-zinc-500"
+        className="h-screen flex items-center justify-center bg-gradient-to-r text-zinc-900 from-gray-300 to-zinc-500 gap-10"
       >
-        <h2 className="text-4xl font-semibold text-gray-900">Sobre mí</h2>
+        <div className="w-3/5 justify-start align-middle text-wrap">
+          <h2 className="text-4xl font-semibold light">Full-stack software developer</h2>
+        </div>
+
+        <div>
+          <ul className="text-bold list-none m-5 border-red-300 border-1 p-3 space-y-3">
+            {technologies.map((tech, index) => (
+              <li key={index} className="">
+                <tech.icon className="w-10 h-10"></tech.icon>
+              </li>
+            ))}
+          </ul>          
+        </div>
+
       </section>
 
       <section
